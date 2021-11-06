2f04e63cef89e67bd3d02ba38e6f17df0eb68301
Pablo Say <19001434@galileo.edu>
2021-11-05 21:42:02 -0600

    Enviroment fixed

--- src/app/rutas/inicio.js
+++ src/app/rutas/inicio.js
@@ -1,4 +1,5 @@
 const conn = require('../../config/database');
+const jwt = require('jsonwebtoken');
 module.exports = (app) => {
     app.get('/inicio/tipo/:tipo/usuario/:usuario/pw/:pw', (req, res, next) => {
         let query = `SELECT usuario FROM ${req.params.tipo} WHERE usuario = '${req.params.usuario}' AND pw = '${req.params.pw}'`;
@@ -7,7 +8,11 @@ module.exports = (app) => {
                 console.log(err);
                 res.json({status: 0, mensaje: "Error en la consulta"});
             } else {
-                res.json({status: 1, mensaje: "Consulta ejecutada", usuario:rows.rows});
+                if(rows.rows.length > 0){
+                    let data = JSON.stringify(rows.rows[0]);
+                    const token = jwt.sign(data, 'xyw');
+                    res.json({status: 1, mensaje: "Consulta ejecutada", usuario:rows.rows, token});
+                }
             }
         });
     });
